11fba98c1aa74f9768c2ae1c6434aa860c715635
jubairskv <jubairskv@gmail.com>
2024-06-25 14:48:54 +0530

    netflix

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -84,7 +84,7 @@ const Login = () => {
       <div className='absolute' >
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/51c1d7f7-3179-4a55-93d9-704722898999/be90e543-c951-40d0-9ef5-e067f3e33d16/IN-en-20240610-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt='logo' />
       </div>
-      <form onSubmit={(e) => e.preventDefault()} className='absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+      <form onSubmit={(e) => e.preventDefault()} className='absolute p-12 bg-black w-3/12 my-36  mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80' >
         <h1 className='font-bold text-3xl py-4 '>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
         {!isSignInForm &&
           <input
